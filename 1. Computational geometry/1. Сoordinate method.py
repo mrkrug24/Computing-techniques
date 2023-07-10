d0c9a1af9b0d0fbb4c7f7c2a676d9816093e2482
mrkrug24 <138400705+mrkrug24@users.noreply.github.com>
2023-07-10 18:25:29 +0300

    Add files via upload

---
+++ 1. Computational geometry/1. Сoordinate method.py
@@ -0,0 +1,46 @@
+a = input().split()
+a[0] = float(a[0])
+a[1] = float(a[1])
+
+b = input().split()
+b[0] = float(b[0])
+b[1] = float(b[1])
+
+c = input().split()
+c[0] = float(c[0])
+c[1] = float(c[1])
+
+d = input().split()
+d[0] = float(d[0])
+d[1] = float(d[1])
+
+def line(x, y):
+    return ((x[0] - y[0])**2 + (x[1] - y[1])**2)**(1/2)
+  
+def area(x, y, z):
+    return (((x+y+z)/2) * ((x+y+z)/2 - x) * ((x+y+z)/2 - y) * ((x+y+z)/2 - z))**0.5
+  
+ab = line(a, b)
+bc = line(b, c)
+ac = line(a, c)
+
+ad = line(a, d)
+bd = line(b, d)
+cd = line(c, d)
+
+S0 = area(ab, bc, ac)
+S1 = area(ab, bd, ad)
+S2 = area(bd, bc, cd)
+S3 = area(ad, cd, ac)
+
+k = 0
+
+if abs(S0 - S1 - S2 - S3) < 0.001:
+    k = 1
+    print("Yes")
+
+if abs(S0 - S1 - S2 - S3) > 0.001:
+    print("No")
+
+if S1 == 0 or S2 == 0 or S3 == 0 and k == 1:
+    print("The point D lies on the side of the triangle ABC")
